b3062a5971a0e11583fbb167c449aa488d7f0583
Henrique Guerra <henriquebg_99@hotmail.com>
2022-09-16 16:35:24 +0100

    Benchmark support for exercises with no examples

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,19 +1,21 @@
 import System.Process (readCreateProcessWithExitCode, shell)
 import System.IO (openFile, hGetLine, hClose, IOMode(ReadMode))
 import Text.Printf (printf)
+import System.Directory (removePathForcibly, createDirectory)
 
-exercises :: [(String, String)]
-exercises = [("[Int] -> [Int]", "[([[1, 2, 3]], [2, 3, 4])]")]
+exercises :: [(String, Maybe String)]
+exercises = [("[Int] -> [Int]", Just "[([[1, 2, 3]], [2, 3, 4])]")]
 
 
-execExercise :: String -> String -> String -> IO ( Double -- ghc
+execExercise :: String -> Maybe String -> String -> IO ( Double -- ghc
                                                  , Double -- match
                                                  , Double -- solver
                                                  )
-execExercise ty exs log = do
+execExercise ty mexs log = do
   -- run exercise
-  let cmd = printf "timeout 60s stack exec -- hplus \"%s\" --example=\"%s\" --cnt=100 --out=%s" ty exs log 
-  readCreateProcessWithExitCode (shell cmd) ""
+
+  readCreateProcessWithExitCode (shell $ command ty mexs) ""
+  putStrLn $ command ty mexs
   -- read results
   f <- openFile log ReadMode
   hGetLine f -- read solution
@@ -21,9 +23,16 @@ execExercise ty exs log = do
   hClose f
   let stats = (map read $ words statsLine) :: [Double]
   return (stats !! 0, stats !! 1, stats !! 2)
+  where
+    command :: String -> Maybe String -> String
+    command ty Nothing = printf "timeout 60s stack exec -- hplus \"%s\" --cnt=100 --out=%s" ty log
+    command ty (Just exs) = printf "timeout 60s stack exec -- hplus \"%s\" --example=\"%s\" --cnt=100 --out=%s" ty exs log 
 
 main :: IO ()
 main = do
+  removePathForcibly "bench-logs"
+  createDirectory "bench-logs"
+
   let withIndex = zipWith (\(x, y) z -> (x, y, z)) exercises ((iterate (+1) 1) :: [Int])
   stats <- mapM (\(x, y, z) -> execExercise x y ("bench-logs/" ++ show z ++ ".log")) withIndex
   print stats
