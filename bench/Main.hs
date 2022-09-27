3d8786a59a5f6f753611fa5be3c3d414f7ae8f26
Henrique Guerra <henriquebg_99@hotmail.com>
2022-09-27 15:58:26 +0100

    change benchmark to run only new problems

--- bench/Main.hs
+++ bench/Main.hs
@@ -7,7 +7,7 @@ import Data.Aeson (encode)
 import Data.ByteString.Lazy.Char8 (unpack)
 
 logsDir :: String
-logsDir = "bench-logs-orig"
+logsDir = "bench-logs-examples"
 
 resultsFile :: String
 resultsFile = "results.log"
@@ -17,50 +17,20 @@ count = 5
 
 exercises :: [(String, String, [Example])]
 exercises = [
-  ("firstRight", "[Either a b] -> Either a b", []),
-  ("firstKey", "[(a, b)] -> a", []),
-  ("flatten", "[[[a]]] -> [a]", []),
-  ("repl-funcs", "(a -> b) -> Int -> [a -> b]", []),
-  ("containsEdge", "Int -> (Int, Int) -> Bool", []),
-  ("multiApp", "(a -> b -> c) -> a -> c", []),
-  ("appendN", "Int -> [a] -> [a]", []),
-  ("pipe", "[a -> a] -> (a -> a)", []),
-  ("intToBS", "Int64 -> ByteString", []),
-  ("cartProduct", "[a] -> [b] -> [[(a, b)]]", []),
-  ("applyNtimes", "(a -> a) -> a -> Int -> a", []),
-  ("firstMatch", "[a] -> (a -> Bool) -> a", []),
-  ("mbElem", "Eq a => a -> [a] -> Maybe a", []),
-  ("mapEither", "(a -> Either b c) -> [a] -> ([b], [c])", []),
-  ("hoogle01", "(a -> b) -> [a] -> b", []),
-  ("zipWithResult", "(a -> b) -> [a] -> [(a, b)]", []),
-  ("splitStr", "String -> Char -> String", []),
-  ("lookup", "[(a, b)] -> a -> b", []),
-  ("fromFirstMaybes", "a -> [Maybe a] -> a", []),
-  ("map", "(a -> b) -> [a] -> [b]", []),
-  ("maybe", "Maybe a -> a -> Maybe a", []),
-  ("rights", "[Either a b] -> Either a [b] ", []),
-  ("mbAppFirst", "b -> (a -> b) -> [a] -> b", []),
-  ("mergeEither", "Either a (Either a b) -> Either a b", []),
-  ("test", "Bool -> a -> Maybe a", []),
-  ("multiAppPair", "(a -> b, a -> c) -> a -> (b, c)", []),
-  ("splitAtFirst", "a -> [a] -> ([a], [a])", []),
-  ("2partApp", "(a->b)->(b->c)->[a]->[c]", []),
-  ("areEq", "Eq a => a -> a -> Maybe a", []),
-  ("eitherTriple", "Either a b -> Either a b -> Either a b", []),
-  ("mapMaybes", "(a -> Maybe b) -> [a] -> Maybe b", []),
-  ("head-rest", "[a] -> (a, [a])", []),
-  ("appBoth", "(a -> b) -> (a -> c) -> a -> (b, c)", []),
-  ("applyPair", "(a -> b, a) -> b", []),
-  ("resolveEither", "Either a b -> (a->b) -> b", []),
-  ("head-tail", "[a] -> (a,a)", []),
-  ("indexesOf", "([(a,Int)] -> [(a,Int)]) -> [a] -> [Int] -> [Int]", []),
-  ("app3", "(a -> b -> c -> d) -> a -> c -> b -> d", []),
-  ("both", "(a -> b) -> (a, a) -> (b, b)", []),
-  ("takeNdropM", "nt -> Int -> [a] -> ([a], [a])", []),
-  ("firstMaybe", "[Maybe a] -> a", []),
-  ("mbToEither", "Maybe a -> b -> Either a b", []),
-  ("pred-match", "[a] -> (a -> Bool) -> Int", []),
-  ("singleList", "Int -> [Int]", [])
+  ("mapAdd", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[2, 3, 4]"}]),
+  ("mapSquare", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[1, 4, 9]"}]),
+  ("appendConst", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[1, 2, 3, 4]"}]),
+  ("filterDiff", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[1, 3]"}]),
+  ("listIntersect", "[Int] -> [Int] -> [Int]", [Example {inputs = ["[0, 2, 4]", "[2, 4, 6"], output = "[2, 4]"}]),
+  ("indexConst", "[Int] -> Int", [Example {inputs = ["[1, 2, 3]"], output = "3"}]),
+  ("allGreaterThan", "[Int] -> Bool", [Example {inputs = ["[2, 3, 4]"], output = "Data.Bool.True"}, 
+                              Example {inputs = ["[2, 1, 4]"], output = "Data.Bool.False"}]),
+  ("dropConst", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3, 4, 5"], output = "[3, 4, 5]"}]),
+  ("filterGreaterThan", "[Int] -> [Int]", [Example {inputs = ["[2, 3, 4, 5, 6]"], output = "[5, 6]"}]),
+  ("filterPairs", "[(Int, Int)] -> [(Int, Int)]", [Example {inputs = ["[(1, 2), (3, 3), (4, 5)]"], output = "[(3, 3)"}]),
+  ("filterEq", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 1, 3, 4, 4]"], output = "[1, 1]"}]),
+  ("replicateConst", "[Int]", [Example {inputs = [], output = "[3, 3, 3, 3, 3"}]),
+  ("addElemsTwoLists", "[Int] -> [Int] -> [Int]", [Example {inputs = ["[1, 2, 3]", "[3, 4, 5"], output = "[4, 6, 8]"}])
   ]
 
 -- example with 
@@ -70,9 +40,9 @@ execExercise :: (String, String, [Example]) -> IO (String, Maybe Double) -- tota
 execExercise (name, ty, mexs) = do
   -- run exercise
   let log = logsDir ++ "/" ++ name ++ ".log"
-  readCreateProcessWithExitCode (shell $ command ty mexs log) ""
   hPutStrLn stderr $ command ty mexs log
   hFlush stderr
+  readCreateProcessWithExitCode (shell $ command ty mexs log) ""
   -- read results
   file <- readFile log
   let ls = lines file
