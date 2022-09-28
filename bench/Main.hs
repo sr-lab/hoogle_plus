a2bb68017985595a78800b52bf10b20f835481d0
Henrique Guerra <henriquebg_99@hotmail.com>
2022-09-28 18:35:48 +0100

    add new examples

--- bench/Main.hs
+++ bench/Main.hs
@@ -21,16 +21,20 @@ exercises = [
   ("mapSquare", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[1, 4, 9]"}]),
   ("appendConst", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[1, 2, 3, 4]"}]),
   ("filterDiff", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3]"], output = "[1, 3]"}]),
+  ("getFirstOnes", "[Int] -> [Int]", [Example {inputs = ["[1, 1, 0, 1, 2]"], output = "[1, 1]"}]),
+  
+  -- stackoverflow
+  ("removeFirstOnes", "[Int] -> [Int]", [Example {inputs = ["[1, 1, 0, 1, 2]"], output = "[0, 1, 2]"}]),
   ("listIntersect", "[Int] -> [Int] -> [Int]", [Example {inputs = ["[0, 2, 4]", "[2, 4, 6"], output = "[2, 4]"}]),
   ("indexConst", "[Int] -> Int", [Example {inputs = ["[1, 2, 3]"], output = "3"}]),
   ("allGreaterThan", "[Int] -> Bool", [Example {inputs = ["[2, 3, 4]"], output = "Data.Bool.True"}, 
                               Example {inputs = ["[2, 1, 4]"], output = "Data.Bool.False"}]),
-  ("dropConst", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3, 4, 5"], output = "[3, 4, 5]"}]),
-  ("filterGreaterThan", "[Int] -> [Int]", [Example {inputs = ["[2, 3, 4, 5, 6]"], output = "[5, 6]"}]),
+  ("dropConst", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 3, 4, 5]"], output = "[3, 4, 5]"}]),
+  ("filterGreaterThan", "[Int] -> [Int]", [Example {inputs = ["[2, 0, 1, 3]"], output = "[2, 3]"}]),
   ("filterPairs", "[(Int, Int)] -> [(Int, Int)]", [Example {inputs = ["[(1, 2), (3, 3), (4, 5)]"], output = "[(3, 3)"}]),
   ("filterEq", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 1, 3, 4, 4]"], output = "[1, 1]"}]),
-  ("replicateConst", "[Int]", [Example {inputs = [], output = "[3, 3, 3, 3, 3"}]),
-  ("addElemsTwoLists", "[Int] -> [Int] -> [Int]", [Example {inputs = ["[1, 2, 3]", "[3, 4, 5"], output = "[4, 6, 8]"}])
+  ("replicateConst", "[Int]", [Example {inputs = [], output = "[3, 3, 3, 3, 3]"}]),
+  ("addElemsTwoLists", "[Int] -> [Int] -> [Int]", [Example {inputs = ["[1, 2, 3]", "[3, 4, 5]"], output = "[4, 6, 8]"}])
   ]
 
 -- example with 
