a1aece956569e0c79b8dcaff3d44b19053305984
Henrique Guerra <henriquebg_99@hotmail.com>
2022-10-29 11:33:50 +0100

    change benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -87,12 +87,12 @@ exercises2 = [
   ("getFirstOnes", "[Int] -> [Int]", [Example {inputs = ["[1, 1, 0, 1, 2]"], output = "[1, 1]"}]),
   
   -- stackoverflow
-  ("removeFirstOnes", "[Int] -> [Int]", [Example {inputs = ["[1, 1, 0, 1, 2]"], output = "[0, 1, 2]"}]),
+  ("removeFirstOnes", "[Int] -> [Int]", [Example {inputs = ["[1, 1, 0, 0, 1, 2]"], output = "[0, 1, 2]"}]),
   ("listIntersect", "[Int] -> [Int] -> [Int]", [Example {inputs = ["[0, 2, 4]", "[2, 4, 6"], output = "[2, 4]"}]),
   ("indexConst", "[Int] -> Int", [Example {inputs = ["[1, 2, 3]"], output = "3"}]),
   ("allGreaterThan", "[Int] -> Bool", [Example {inputs = ["[2, 3, 4]"], output = "Data.Bool.True"}, 
                               Example {inputs = ["[2, 1, 4]"], output = "Data.Bool.False"}]),
-  ("dropConst", "[Int] -> [Int]", [Example {inputs = ["[0, 4, 4, 3]"], output = "[4, 3]"}]),
+  ("dropConst", "[Int] -> [Int]", [Example {inputs = ["[0, 0, 4, 4, 3]"], output = "[4, 3]"}]),
   ("filterGreaterThan", "[Int] -> [Int]", [Example {inputs = ["[2, 0, 1, 3]"], output = "[2, 3]"}]),
   ("filterPairs", "[(Int, Int)] -> [(Int, Int)]", [Example {inputs = ["[(1, 2), (3, 3), (4, 5)]"], output = "[(3, 3)"}]),
   ("filterEq", "[Int] -> [Int]", [Example {inputs = ["[1, 2, 1, 3, 4, 4]"], output = "[1, 1]"}]),
