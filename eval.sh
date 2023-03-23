7d2ab42eff80e2482e1c37df39203325b5061910
Henrique Guerra <henriquebg_99@hotmail.com>
2023-03-23 20:57:33 +0000

    fix problem with flags in scirpts

--- eval.sh
+++ eval.sh
@@ -1,20 +1,20 @@
 FLAG_SET_1=S1
 FLAG_SET_2=S2
 
-if (test $# -ne 0 && test $# -ne 1) || (test $# -eq 1 && test $1 -ne $FLAG_SET_1 && test $1 -ne $FLAG_SET_2) 
+if (test $# -ne 0 && test $# -ne 1) || (test $# -eq 1 && test $1 != $FLAG_SET_1 && test $1 != $FLAG_SET_2) 
 then 
     echo "Usage: bash $0 [$FLAG_SET_1|$FLAG_SET_2]"
     exit 1
 fi
 
-if test $# -eq 0 || (test $# -eq 1 && test $1 -eq $FLAG_SET_1)
+if test $# -eq 0 || (test $# -eq 1 && test $1 = $FLAG_SET_1)
 then 
     echo "44 benchmark on original Hoogle+"
     cd hoogle_plus_orig/
     sh eval_orig.sh $*
     cd ..
 fi
-if test $# -eq 0 || (test $# -eq 1 && test $1 -eq $FLAG_SET_2)
+if test $# -eq 0 || (test $# -eq 1 && test $1 = $FLAG_SET_2)
 then
     echo "26 benchmark on original Hoogle+ with examples"
     cd hoogle_plus_examp/
